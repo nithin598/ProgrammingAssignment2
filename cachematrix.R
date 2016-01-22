4809362f3b3994147507aa80073dd20ab9f160ae
Prabhu Nithin Saripudi <nithin598@gmail.com>
2016-01-22 23:57:51 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I have created two functions makeCacheMatrix() and cacheSolve().
+## 
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix will be used to assign to parent variables and returns a list. 
 
+makeCacheMatrix <- function(myMatrix = matrix()) {
+        invMat <- NULL                      # Initializing the inverse matrix
+        set <- function(y) {                      
+                myMatrix <<- y
+                invMat <<- NULL
+        }
+        get <- function() myMatrix
+        setInvMat <- function(matInverse) invMat <<- matInverse
+        getInvMat <- function() invMat
+        list(set = set, get = get, setInvMat = setInvMat,getInvMat = getInvMat)
 }
 
-
-## Write a short comment describing this function
+## Computes the inverse of the matrix returned by makeCacheMatrix above.
+## If the inverse of the matrix was already calculated then value gets retrived from Cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invMat <- x$getInvMat()
+        if(!is.null(invMat)) {
+                message("getting cached inverse matrix")
+                return(invMat)
+        }
+        dataMatrix <- x$get()
+        invMat <- solve(dataMatrix, ...)
+        x$setInvMat(invMat)
+        invMat
 }
+## Implementation
+
+x <- matrix(1:4, nrow = 2, ncol = 2, byrow = TRUE)
+mat<-makeCacheMatrix(x)
+cacheSolve(mat) ## uncached solution
+cacheSolve(mat) ## cached solution as it was already calculated before.
+
